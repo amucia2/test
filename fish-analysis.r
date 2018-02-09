788358c13f279230a146913342b8ef154d3f97cf
Anthony Mucia <amucia2@unl.edu>
2018-02-09 10:06:26 -0600

    Changed name of csv file and changed first line of code to get read new file name

--- fish-analysis.r
+++ fish-analysis.r
@@ -1,12 +1,9 @@
 .libPaths("P:/RLibrary")
 library(tidyverse)
 
-fish_data <- read.csv("Gaeta_etal_CLC_data.csv")
+fish_data <- read.csv("Gaeta_etal_CLC_data_1.csv")
 
 fish_data %>% 
-  mutate(length_cat = ifelse(length>300,"big","small")) -> fish_data_cat
-
-fish_data %>%
+  mutate(length_cat = ifelse(length>300,"big","small")) %>% 
   filter(scalelength >= 1) -> fish_data_cat
-
-nrow(fish_data_cat)
+  
