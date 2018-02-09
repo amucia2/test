4128a8c907e8267cb74e6b57e29371c6813d8b6a
Anthony Mucia <amucia2@unl.edu>
2018-02-09 09:52:32 -0600

    Remove rows with scalelength less than 1mm

--- fish-analysis.r
+++ fish-analysis.r
@@ -5,3 +5,8 @@ fish_data <- read.csv("Gaeta_etal_CLC_data.csv")
 
 fish_data %>% 
   mutate(length_cat = ifelse(length>300,"big","small")) -> fish_data_cat
+
+fish_data %>%
+  filter(scalelength >= 1) -> fish_data_cat
+
+nrow(fish_data_cat)
