701fb640df53c985067f624d03541294ef15b3ab
Anthony Mucia <amucia2@unl.edu>
2018-02-09 10:36:33 -0600

    Added scatterplot of length and scalelength

--- fish-analysis.r
+++ fish-analysis.r
@@ -5,5 +5,10 @@ fish_data <- read.csv("Gaeta_etal_CLC_data_1.csv")
 
 fish_data %>% 
   mutate(length_cat = ifelse(length>300,"big","small")) %>% 
-  filter(scalelength >= 1) -> fish_data_cat
+  filter(scalelength >= 1) -> fish_data_cat 
   
+fish_data_cat %>% 
+  filter(annnumber == "EDGE") %>% 
+ggplot(fish_data_cat, aes(length, scalelength)) +
+  geom_point(lakeid))
+
